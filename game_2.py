fefdf6c2c4bedeed28caf5a6083da17abbb040a9
mohammadalinouri <mohammadalinouri608@gmail.com>
2022-07-22 17:15:16 +0430

    couple of changes

--- game_2.py
+++ game_2.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 
+pygame.font.init()
 FPS= 60
 WHITE=(255, 255, 255)
 BLACK=(0, 0, 0,)
@@ -10,6 +11,7 @@ WIN=pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('online race')
 border=pygame.Rect(880, 0, 10, 500)
 road=pygame.image.load(os.path.join('Asset','road.png'))
+font=pygame.font.SysFont('comicsans', 50)
 
 #importing player image
 cyan_image=pygame.image.load(os.path.join('Asset','cyan.png'))
@@ -17,6 +19,10 @@ pink_image=pygame.image.load(os.path.join('Asset','pink.png'))
 red_image=pygame.image.load(os.path.join('Asset','red.png'))
 yellow_image=pygame.image.load(os.path.join('Asset','yellow.png'))
 
+def draw_winner(txt):
+    draw_text=font.render(txt, 1, BLACK)
+    WIN.blit(draw_text, (WIDTH//2-draw_text.get_width()//2 , HEIGHT//2-draw_text.get_height()//2))
+    pygame.display.update()
 
 
 def draw_window(cyan, pink, red, yellow):
@@ -30,19 +36,19 @@ def draw_window(cyan, pink, red, yellow):
 
 
     if cyan.x>=800:
-        pygame.display.set_caption(' cyan just won the game')
+        draw_winner('cyan just won the game')
         time.sleep(3)
         quit()
     if pink.x>=800:
-        pygame.display.set_caption('pink just won the game')
+        draw_winner('pink just won the game')
         time.sleep(3)
         quit()
     if red.x>=800:
-        pygame.display.set_caption('red just won the game')
+        draw_winner('red just won the game')
         time.sleep(3)
         quit()
     if yellow.x>=800:
-        pygame.display.set_caption('yellow just won the game')
+        draw_winner('yellow just won the game')
         time.sleep(3)
         quit()
 
